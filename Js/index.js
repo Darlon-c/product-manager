557384f431a4136d1282c5898926aeca553a8244
Darlon <darloncosta050@gmail.com>
2026-03-06 16:19:39 -0300

    att

---
+++ Js/index.js
@@ -0,0 +1,13 @@
+const productName = document.getElementById('productName')
+const productPrice = document.getElementById('productPrice')
+const productQuantity = document.getElementById('productQuantity')
+const productCategories = document.getElementById('categories')
+const saveProduct = document.getElementById('saveProduct')
+
+
+
+
+
+
+
+
