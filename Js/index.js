eb882b72b9867d2f861e7af09422e2ba1de68b52
Darlon <darloncosta050@gmail.com>
2026-03-06 16:34:24 -0300

    function add

--- Js/index.js
+++ Js/index.js
@@ -1,13 +1,41 @@
-const productName = document.getElementById('productName')
-const productPrice = document.getElementById('productPrice')
-const productQuantity = document.getElementById('productQuantity')
-const productCategories = document.getElementById('categories')
-const saveProduct = document.getElementById('saveProduct')
+const productName = document.getElementById("productName");
+const productPrice = document.getElementById("productPrice");
+const productQuantity = document.getElementById("productQuantity");
+const productCategories = document.getElementById("categories");
+const saveProduct = document.getElementById("saveProduct");
 
+let listProducts = [];
+let idCount = 1;
 
+function newProduct() {
+  if (productName.value === "") {
+    Swal.fire("Informe o nome do produto");
+    return;
+  } else if (productPrice.value === "") {
+    Swal.fire("Informe o preço do produto");
+    return;
+  } else if (productQuantity.value === "") {
+    Swal.fire("Informe a quantidade do produto");
+    return;
+  }
 
+  const product = {
+    id: idCount,
+    name: productName.value,
+    price: Number(productPrice.value),
+    quantity: Number(productQuantity.value),
+    category: productCategories.value,
+    totalValue: Number(productPrice.value * productQuantity.value)
+  };
 
+  listProducts.push(product);
+  idCount++
 
+  console.log(listProducts)
 
+  productName.value = "";
+  productPrice.value = "";
+  productQuantity.value = " ";
+}
 
-
+saveProduct.addEventListener("click", newProduct);
