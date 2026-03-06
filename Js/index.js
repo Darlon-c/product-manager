81071f1150498ee7f30c79c3125e1e55caca482c
Darlon <darloncosta050@gmail.com>
2026-03-06 16:45:31 -0300

    att

--- Js/index.js
+++ Js/index.js
@@ -3,6 +3,7 @@ const productPrice = document.getElementById("productPrice");
 const productQuantity = document.getElementById("productQuantity");
 const productCategories = document.getElementById("categories");
 const saveProduct = document.getElementById("saveProduct");
+const resumeProducts = document.getElementById("resumeProducts");
 
 let listProducts = [];
 let idCount = 1;
@@ -25,17 +26,34 @@ function newProduct() {
     price: Number(productPrice.value),
     quantity: Number(productQuantity.value),
     category: productCategories.value,
-    totalValue: Number(productPrice.value * productQuantity.value)
+    totalValue: Number(productPrice.value * productQuantity.value),
   };
 
   listProducts.push(product);
-  idCount++
+  idCount++;
+  renderProducts();
 
-  console.log(listProducts)
+  console.log(listProducts);
 
   productName.value = "";
   productPrice.value = "";
-  productQuantity.value = " ";
+  productQuantity.value = "";
+}
+
+function renderProducts() {
+  const render = listProducts.map((product) => {
+    return `
+      <div>
+        <p>Nome: ${product.name}</p>
+        <p>Preço: R$${product.price.toFixed(2)}</p>
+        <p>Quantidade: ${product.quantity}</p>
+        <p>Categoria: ${product.category}</p>
+         <p>Valor total: R$${product.totalValue.toFixed(2)}</p>
+      </div>
+    `;
+  });
+
+  resumeProducts.innerHTML = render.join("");
 }
 
 saveProduct.addEventListener("click", newProduct);
